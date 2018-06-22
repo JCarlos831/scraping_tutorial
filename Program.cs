9b9a33ac806fd30cace439001cb9e7ac2c69a2fe
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-22 16:15:17 -0400

    Able to login to yahoo finance site

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// https://www.youtube.com/watch?v=cNKi0Pl5wbc
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace scraping_tutorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F";
+
+            driver.Manage().Window.Maximize();
+            
+            driver.FindElement(By.Id("login-username")).SendKeys("testfinance@yahoo.com" + Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("login-passwd")).Click();
+            driver.FindElement(By.Id("login-passwd")).SendKeys("3eggwhites6" + Keys.Enter);
+        }
+    }
+}
